0f88245ba81f8bddf2eafb178de6092e9ebc6351
clintjeez <clintjeez@gmail.com>
2020-05-04 17:58:47 +0100

    new update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// Note: this code block does same thing with the one below and 
+// both cant be use at the same it just a similar controller with 
+// a different coding style
+
+// UI Mode controller
 // const switcher = document.getElementById('input')
 // const uiMode = () => {
 //     if(switcher.checked){
@@ -9,8 +14,13 @@
 // switcher.addEventListener('change', uiMode);
 
 
-const switcher = document.getElementById('input')
-switcher.addEventListener('change', uiMode= () =>{
-    switcher.checked ? style.href='/styles/dark-theme.css' 
-    : style.href='/styles/light-theme.css'
- })
+// UI Mode controller
+const switcher = document.getElementById("input");
+switcher.addEventListener(
+  "change",
+  (uiMode = () => {
+    switcher.checked
+      ? (style.href = "/styles/dark-theme.css")
+      : (style.href = "/styles/light-theme.css");
+  })
+);
